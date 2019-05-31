75c4467e282e440fe8989c3b67f59d7d8d2f7c12
Zach Parker <parkerz410@gmail.com>
2019-05-31 16:28:29 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,16 @@
-# your code goes here
+require "pry"
+
+def begins_with_r (array)
+  array.all? { |str| str.start_with?("r", "R")  }
+end
+
+def contain_a (array)
+  new_array = []
+  array.each do |str|
+    # binding.pry
+    if str.include?("a")
+      new_array << str
+    end
+  end
+  return new_array
+end
